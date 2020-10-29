75479ccd29e0e046a824c4c97a2e17ad11f6d3fa
LeeDongHyun <linear14@naver.com>
2020-10-29 23:19:59 +0900

    LottieExample #001 : Lottie Animation 시작 및 일시정지
    - Animation 시작 및 일시정지 기능 연습
    - Toggle Button (On/Off Switch) 간단한 연습
    
    - playAnimation()
    - cancelAnimation()
    - setMinAndMaxProgress()
    - get/setProgress()
    - lottie_rawRes
    - lottie_loop
    - lottie_autoPlay

---
+++ LottieExample/app/src/main/java/com/dongldh/lottieexample/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.dongldh.lottieexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var switchOn = false
+    var progress = 0.0f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lottie_switch.setOnClickListener {
+            if(switchOn) {
+                progress = lottie_bus.progress
+                lottie_bus.cancelAnimation()
+                lottie_switch.setMinAndMaxProgress(0.5f, 1.0f)
+                lottie_switch.playAnimation()
+            } else {
+                lottie_bus.playAnimation()
+                lottie_bus.progress = progress
+                lottie_switch.setMinAndMaxProgress(0.0f, 0.5f)
+                lottie_switch.playAnimation()
+            }
+            switchOn = !switchOn
+        }
+
+    }
+}
