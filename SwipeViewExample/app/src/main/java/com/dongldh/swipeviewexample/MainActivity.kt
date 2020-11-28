9f57ffdc492b851d216f043aa7e8c6ee8d9d7e98
LeeDongHyun <linear14@naver.com>
2020-11-28 20:14:30 +0900

    SwipeViewExample #003 : 리사이클러뷰 레이아웃 + 아이템 제작

--- SwipeViewExample/app/src/main/java/com/dongldh/swipeviewexample/MainActivity.kt
+++ SwipeViewExample/app/src/main/java/com/dongldh/swipeviewexample/MainActivity.kt
@@ -1,50 +1,11 @@
 package com.dongldh.swipeviewexample
 
-import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.constraintlayout.motion.widget.MotionLayout
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        motion_layout.addTransitionListener(object: MotionLayout.TransitionListener {
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
-            }
-
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-            }
-
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
-            }
-
-            override fun onTransitionCompleted(view: MotionLayout?, setState: Int) {
-                if(setState == R.id.end2) {
-                    motion_layout.animate()
-                        .alpha(0f)
-                        .setDuration(1000)
-                        .setListener(object: Animator.AnimatorListener {
-                            override fun onAnimationRepeat(p0: Animator?) {
-                            }
-
-                            override fun onAnimationEnd(p0: Animator?) {
-                            }
-
-                            override fun onAnimationCancel(p0: Animator?) {
-                            }
-
-                            override fun onAnimationStart(p0: Animator?) {
-                                Toast.makeText(this@MainActivity, "Message Deleted", Toast.LENGTH_SHORT).show()
-                            }
-
-                        })
-                }
-            }
-        })
-
     }
 }
---
+++ SwipeViewExample/app/src/main/java/com/dongldh/swipeviewexample/TestActivity.kt
@@ -0,0 +1,50 @@
+package com.dongldh.swipeviewexample
+
+import android.animation.Animator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_test.*
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+        motion_layout.addTransitionListener(object: MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(view: MotionLayout?, setState: Int) {
+                if(setState == R.id.end2) {
+                    motion_layout.animate()
+                        .alpha(0f)
+                        .setDuration(1000)
+                        .setListener(object: Animator.AnimatorListener {
+                            override fun onAnimationRepeat(p0: Animator?) {
+                            }
+
+                            override fun onAnimationEnd(p0: Animator?) {
+                            }
+
+                            override fun onAnimationCancel(p0: Animator?) {
+                            }
+
+                            override fun onAnimationStart(p0: Animator?) {
+                                Toast.makeText(this@TestActivity, "Message Deleted", Toast.LENGTH_SHORT).show()
+                            }
+
+                        })
+                }
+            }
+        })
+
+    }
+}
